c6a1839c5500f8bf6f7ede3a8933e132cd924c90
Felicity Johnson <felicity.johnson.mail@gmail.com>
2017-09-18 21:23:17 -0400

    set up custom cell & hooked up to collectionVC

--- Bideawee/Bideawee/AnimalCollectionViewCell.swift
+++ Bideawee/Bideawee/AnimalCollectionViewCell.swift
@@ -9,9 +9,16 @@
 import Foundation
 import UIKit
 
+private struct Layout {
+    static let animalPicWidthHeight: CGFloat = 60.0
+    static let descriptionLabelHeight: CGFloat = 50.0
+}
+
 class AnimalCollectionViewCell: UICollectionViewCell {
     
     var animalPic = UIImageView()
+    var nameLabel = UILabel()
+    var descriptionLabel = UILabel()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -25,10 +32,25 @@ class AnimalCollectionViewCell: UICollectionViewCell {
     
     func setUpView() {
         addSubview(animalPic)
+        addSubview(nameLabel)
+        addSubview(descriptionLabel)
+        
+        animalPic.contentMode = .scaleAspectFit
+        
         animalPic.translatesAutoresizingMaskIntoConstraints = false
         animalPic.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
-        animalPic.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
-        animalPic.widthAnchor.constraint(equalTo: self.widthAnchor).isActive = true
-        animalPic.heightAnchor.constraint(equalTo: self.heightAnchor).isActive = true
+        animalPic.topAnchor.constraint(equalTo: self.topAnchor, constant: 20).isActive = true
+        animalPic.widthAnchor.constraint(equalToConstant: Layout.animalPicWidthHeight).isActive = true
+        animalPic.heightAnchor.constraint(equalToConstant: Layout.animalPicWidthHeight).isActive = true
+        
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        nameLabel.topAnchor.constraint(equalTo: animalPic.bottomAnchor, constant: 10).isActive = true
+        nameLabel.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.8).isActive = true
+        
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        descriptionLabel.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+        descriptionLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 10).isActive = true
+        descriptionLabel.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.8).isActive = true
     }
 }
--- Bideawee/Bideawee/ResultsCollectionViewController.swift
+++ Bideawee/Bideawee/ResultsCollectionViewController.swift
@@ -9,22 +9,28 @@
 import Foundation
 import UIKit
 
+//private struct Layout {
+//    static let cellSpacing: CGFloat = 5
+//}
+
 class ResultsCollectionViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
-    private var collectionView: UICollectionView!
+    private let layout = UICollectionViewFlowLayout()
+    private let collectionView: UICollectionView
     var animals = [Animal]()
     
-    var sectionInsets: UIEdgeInsets!
-    var itemSize: CGSize!
-    var referenceSize: CGSize!
-    var numberOfRows: CGFloat!
-    var numberOfColumns: CGFloat!
-    var url: URL!
-    var convertedImage: UIImage!
-    var insetSpacing: CGFloat!
-    var minimumInterItemSpacing: CGFloat!
-    var minimumLineSpacing: CGFloat!
-    let layout = UICollectionViewFlowLayout()
+    init() {
+        let flowLayout                     = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection         = .vertical
+        flowLayout.minimumLineSpacing      = 0
+        collectionView                     = UICollectionView(frame: .zero, collectionViewLayout:flowLayout)
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("Error loading ResultsCollectionViewController")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -32,9 +38,11 @@ class ResultsCollectionViewController: UIViewController, UICollectionViewDelegat
     }
     
     func setUpView() {
+
+        view.backgroundColor = .white
+        
         collectionView.delegate = self
         collectionView.dataSource = self
-        collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
         
         view.addSubview(collectionView)
         collectionView.translatesAutoresizingMaskIntoConstraints = false
@@ -62,84 +70,10 @@ class ResultsCollectionViewController: UIViewController, UICollectionViewDelegat
         
         let imageName = animals[indexPath.item].imageLink
         cell.animalPic.image = UIImage(imageLiteralResourceName: "catPic")
+        cell.nameLabel.text = "Tiffany"
+        cell.descriptionLabel.text = "Loving and kind, Leo is as gentle as it gets. Leo is a one year old male Domestic Shorthair with an exotic look. Leo has a long and illustrious face with big ears. At 11 pounds, he's on the larger side, which just adds to his intriguing appearance. Leo is a wonderful cat, although he can be slightly timid at first. But with a few scratches behind the ears and some sweet words, Leo begins to open up. He'll lean into your hand for more pets and you'll begin to hear a low purr come from his body. Leo is also big fan of scratches on the back, as you can tell from the way his behind begins to rise high in the air. Leo gets along great with other cats and would be a wonderful companion if you already have a feline. If this gentle soul seems like the one for you, come adopt Leo today!"
         cell.layer.borderWidth = 2.0
         cell.layer.borderColor = UIColor.black.cgColor
         return cell
-    }
-    
-    //MARK: Header & Footer Layout Configuration
-    
-    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
-        
-        if kind == UICollectionElementKindSectionHeader {
-            
-            // set up and return a view
-            let headerView = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionElementKindSectionHeader, withReuseIdentifier: "headerCell", for: indexPath)
-            
-            headerView.backgroundColor = UIColor.gray
-            
-            return headerView
-            
-            
-        }
-        
-        if kind == UICollectionElementKindSectionFooter {
-            
-            // set up and return a view
-            
-        }
-        
-        return UICollectionReusableView()
-        
-    }
-    
-    
-    
-    //MARK: Cell Layout Configuration
-    
-    func cellConfig() {
-        let screedWidth = UIScreen.main.bounds.width
-        let screenHeight = UIScreen.main.bounds.height
-        
-        let numOfRows = CGFloat(3.0)
-        let numOfColumns = CGFloat(3.0)
-        
-        insetSpacing = 2
-        minimumInterItemSpacing = 2
-        minimumLineSpacing = 2
-        sectionInsets = UIEdgeInsetsMake(insetSpacing, insetSpacing, insetSpacing, insetSpacing)
-        referenceSize = CGSize(width: screedWidth, height: 80)
-        
-        let totalWidthDeduction = (minimumInterItemSpacing + minimumInterItemSpacing + sectionInsets.right + sectionInsets.left)
-        let totalHeightDeduction = (sectionInsets.right + sectionInsets.left + minimumLineSpacing + minimumLineSpacing)
-        
-        
-        
-        itemSize = CGSize(width: (screedWidth/numOfColumns) - (totalWidthDeduction/numOfColumns), height: (screenHeight - totalHeightDeduction)/3)
-        
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return minimumLineSpacing
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return minimumInterItemSpacing
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return sectionInsets
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return itemSize
-    }
-    
-    //    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
-    //        return referenceSize
-    //    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
-        return referenceSize
     }
 }
